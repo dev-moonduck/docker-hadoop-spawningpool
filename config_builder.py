1c188761d0beb94c624fbfe2c9c7a7014377f794
Moon <munduk.hyun@agoda.com>
2021-05-01 21:57:03 +0700

    Move out config to dedicate file

---
+++ config_builder.py
@@ -0,0 +1,74 @@
+def build_config_from_args(args):
+    return {
+        "dependencyVersions": _componentVersions(args),
+        "instances": _instances(args)
+    }
+
+
+def _componentVersions(args):
+    version = {
+        "java": args.jave_version,
+        "hadoop": args.hadoop_version,
+        "zookeeper": args.zookeeper_version
+    }
+    if args.hive or args.all:
+        version["hive"] = args.hive
+    if args.spark_thrift or args.all:
+        version["spark"] = args.spark_version
+    if args.hue or args.all:
+        version["hue"] = args.hue_version
+    return version
+
+
+def _instances(args):
+    # Required instances
+    all_instances = {
+        "primary-namenode": {
+            "hosts": ["primary-namenode1", "namenode1", "nameservice1", "journalnode1", "resource-manager",
+                      "yarn-history"],
+            "components": ["primary-namenode", "journalnode", "resource-manager", "yarn-history"],
+            "image": "hadoop",
+            "ports": ["9870:9870", "8088:8088"]
+        },
+        "secondary-namenode": {
+            "hosts": ["secondary-namenode1", "namenode2", "journalnode2", "spark-history"],
+            "components": ["secondary-namenode", "journalnode"],
+            "image": "hadoop",
+            "ports": ["9871:9870", "18080:18080"]
+        },
+        "datanode1": {
+            "hosts": ["datanode1", "journalnode2"],
+            "components": ["datanode", "journalnode"],
+            "image": "hadoop",
+            "ports": ["9864:9864"]
+        }
+    }
+
+    # Set optional instances
+    # Add more datanode
+    num_datanode = args.num_datanode
+    for i in range(2, num_datanode + 1):
+        all_instances["datanode" + i] = {
+            "hosts": ["datanode1"],
+            "components": ["datanode"],
+            "ports": [(9864 + i - 1) + ":9864"] # 9865, 9866, ...
+        }
+
+    if args.hive or args.all:
+        all_instances["primary-namenode"]["hosts"] += ["hive-server1", "hive-metastore1"]
+        all_instances["primary-namenode"]["components"] += ["hive-server", "hive-metastore"]
+        all_instances["primary-namenode"]["ports"] += ["10000:10000", "10001:10001", "10002:10002", "9083:9083"]
+        all_instances["primary-namenode"]["image"] = "hive"
+
+    if args.spark_thrift or args.all:
+        all_instances["secondary-namenode"]["hosts"] += ["spark-thrift1"]
+        all_instances["secondary-namenode"]["components"] += ["spark-thrift"]
+        all_instances["secondary-namenode"]["ports"] += ["10010:10000", "10011:10001", "10012:10002"]
+        all_instances["secondary-namenode"]["image"] = "hive"
+
+    if args.hue or args.all:
+        all_instances["secondary-namenode"]["hosts"] += ["hue"]
+        all_instances["secondary-namenode"]["image"] = "hue"
+        all_instances["secondary-namenode"]["ports"] += ["8888:8888"]
+
+    return all_instances
--- main.py
+++ main.py
@@ -1,39 +1,10 @@
 import argparse
 import pathlib
+import config_builder
 
 
 BASE_PATH = pathlib.Path(__file__).parent.absolute()
-IMAGE_LAYER = {
-    "hadoop": 1,
-    "hive": 2,
-    "hue": 3
-}
-SUPPORTED_COMPONENTS = {
-    "primary-namenode": {
-        "image": "hadoop"
-    },
-    "secondary-namenode": {
-        "image": "hadoop"
-    },
-    "datanode": {
-        "image": "hadoop"
-    },
-    "journalnode": {
-        "image": "hadoop"
-    },
-    "hive-server": {
-        "image": "hive"
-    },
-    "hive-metastore": {
-        "image": "hive"
-    },
-    "spark-thrift": {
-        "image": "hive"
-    },
-    "hue": {
-        "image": "hue"
-    }
-}
+
 
 
 def parse_arg():
@@ -57,64 +28,10 @@ def parse_arg():
     return parser.parse_args()
 
 
-def componentVersions(args):
-    version = {
-        "java": args.jave_version,
-        "hadoop": args.hadoop_version,
-        "zookeeper": args.zookeeper_version
-    }
-    if args.hive or args.all:
-        version["hive"] = args.hive
-    if args.spark_thrift or args.all:
-        version["spark"] = args.spark_version
-    if args.hue or args.all:
-        version["hue"] = args.hue_version
-    return version
-
-
-def get_instances(args):
-    # Required instances
-    instances = {
-        "primary-namenode": {
-            "hosts": ["primary-namenode1", "namenode1", "nameservice1", "journalnode1"],
-            "components": ["primary-namenode", "journalnode"]
-        },
-        "secondary-namenode": {
-            "hosts": ["secondary-namenode1", "namenode2", "journalnode2"],
-            "components": ["secondary-namenode", "journalnode"]
-        },
-        "datanode1": {
-            "hosts": ["datanode1", "journalnode2"],
-            "components": ["datanode", "journalnode"]
-        }
-    }
-
-    # Set optional instances
-    # Add more datanode
-    num_datanode = args.num_datanode
-    for i in range(2, num_datanode + 1):
-        instances["datanode" + i] = {
-            "hosts": ["datanode1"],
-            "components": ["datanode"]
-        }
-
-    if args.hive or args.all:
-        instances["primary-namenode"]["hosts"] += ["hive-server1", "hive-metastore1"]
-        instances["primary-namenode"]["components"] += ["hive-server", "hive-metastore"]
-
-    if args.spark_thrift or args.all:
-        instances["secondary-namenode"]["hosts"] += ["spark-thrift1"]
-        instances["secondary-namenode"]["components"] += ["spark-thrift"]
-
-    if args.hue or args.all:
-        instances["secondary-namenode"]["hosts"] += ["hue"]
-
-    return instances
-
-
 def run():
     args = parse_arg()
-    print(get_instances(args))
+    template_data = config_builder.get_instances(args)
+    print(template_data)
 
 
 if __name__ == '__main__':
